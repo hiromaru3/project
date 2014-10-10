177f4d6a5b42ced48a1aa5fb8c9a3071965831d2
hawkkk <aaaa@example.com>
2014-10-10 12:14:06 +0900

    a

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const int MOD_ALT = 0x0001;
+        const int MOD_CONTROL = 0x0002;
+        const int MOD_SHIFT = 0x0004;
+        const int WM_HOTKEY = 0x0312;
+
+        const int HOTKEY_ID = 0x0001;  // 0x0000～0xbfff 内の適当な値でよい
+        const int HOTKEY2_ID = 0x0002;
+
+        [DllImport("user32.dll")]
+        extern static int RegisterHotKey(IntPtr HWnd, int ID, int MOD_KEY, int KEY);
+        [DllImport("user32.dll")]
+        extern static int UnregisterHotKey(IntPtr HWnd, int ID);
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_HOTKEY)
+            {
+                if ((int)m.WParam == HOTKEY_ID)
+                {
+                    if (WindowState == FormWindowState.Normal)
+                    {
+                        this.WindowState = FormWindowState.Minimized;
+                        ShowInTaskbar = false;
+                    }
+                    else if (WindowState == FormWindowState.Minimized)
+                    {
+                        this.WindowState = FormWindowState.Normal;
+                        ShowInTaskbar = true;
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            ShowInTaskbar = false;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            RegisterHotKey(this.Handle, HOTKEY_ID, MOD_ALT, (int)Keys.A);
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                this.contextMenuStrip1.Show();
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                this.WindowState = FormWindowState.Normal;
+                ShowInTaskbar = true;
+            }
+        }
+
+        private void ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                ShowInTaskbar = false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+        }
+    }
+}
